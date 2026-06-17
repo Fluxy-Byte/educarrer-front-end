a21c1c17f7e1c05f3ca3072768d1f5953eb02f4a
Gabriel <lopes.biel2588@gmail.com>
2026-06-17 19:23:51 -0300

    FEATURE: Corrindo cor de fundo do loading

--- components/loading.tsx
+++ components/loading.tsx
@@ -9,7 +9,7 @@ export default function Loading({ loading, message }: Props) {
 
   if (loading) {
     return (
-      <div className="flex fixed items-center bg-neutral-300/40 justify-center w-full h-full top-0 left-0 z-999">
+      <div className="flex fixed items-center bg-neutral-300/60 justify-center w-full h-full top-0 left-0 z-999">
         <div className="text-center">
           <div className="inline-block h-8 w-8 text-black animate-spin rounded-full border-4 border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]" />
           <p className="mt-4 text-black">{
